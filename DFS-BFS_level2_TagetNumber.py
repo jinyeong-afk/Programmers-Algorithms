0642416cb7721385383055694ab5988e11bac7c2
jinyeong-afk <xoeh7002@naver.com>
2021-07-08 01:56:18 +0900

    깊이/너비 우선 탐색 Level 2 타겟 넘버(X) - DFS/BFS 이해도 부족

---
+++ DFS-BFS_level2_TagetNumber.py
@@ -0,0 +1,9 @@
+def solution(numbers, target):
+    sup = [0]
+    for i in numbers:
+        sub = []
+        for j in sup:
+            sub.append(j+i)
+            sub.append(j-i)
+        sup = sub
+    return sup.count(target)
